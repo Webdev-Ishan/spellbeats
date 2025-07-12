06d161a8fcccb6f20fdefb3eeffa8c2dad801352
Ishan Saini <ishansaini0105@gmail.com>
2025-07-12 11:54:50 +0530

    Boiler plate for next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
+import { ToastContainer } from "react-toastify";
+import Provider from "@/components/Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider>
+          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={true}
+            closeOnClick
+            pauseOnHover
+            theme="dark"
+          />
+        </Provider>
       </body>
     </html>
   );
