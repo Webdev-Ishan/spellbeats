46d54985d36e308d73ba0fbb2415eafd800d12f2
Ishan Saini <ishansaini0105@gmail.com>
2025-07-22 10:47:47 +0530

    The schema is changed

---
+++ prisma/migrations/20250722051719_removed_the_cloudinary_from_schema/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `cloudinaryURL` on the `streams` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "streams" DROP COLUMN "cloudinaryURL";
